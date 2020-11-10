1cfb33f0a338dea384403bbbf08f053571075706
Sergei Chusovskoy <sergei.chusovskoy@gmail.com>
2020-11-11 01:52:45 +0300

    Update fpIntro

--- src/playground/fpIntro.js
+++ src/playground/fpIntro.js
@@ -1,22 +1,106 @@
-import { pipe as Rpipe } from 'ramda'
-
-// Declarative programming
-// Functional programming is declarative: it doesn’t reveal details of implementation.
-
-// Immutability
-// Moore's law established that the number of transistors on integrated circuits
-// would double every two years. However, the limit was reached for the year
-// 2002. Later, multicore processors appeared to keep the speed increases.
-// Parallelizable code is the only way to take advantage of them.
-
 const notCurryFn = (a, b) => a + b
 const curryFn = a => b => a + b
 
+const multiply = a => b => a * b
+const multiplyByTwo = multiply(2)
+
+const gt = a => b => b > a
+const lt = a => b => b < a
+const greaterThenTwo = gt(2)
+
+const numbers = [1, 2, 4, -2, 5]
+let result = numbers.filter(greaterThenTwo)
+console.log(result)
+
+result = numbers.filter(greaterThenTwo).map(multiplyByTwo)
+console.log(result)
+
+//
+
 const compose = (...fns) => (...args) =>
   fns.reduceRight((acc, fn) => [fn.apply(null, acc)], args)[0]
-const pipe = (...args) => Rpipe(...args)()
 
-const fibo = n => (n < 2 ? 1 : fibo(n - 2) + fibo(n - 1))
+let pipe = (...fns) => (...args) =>
+  fns.reduce((acc, fn) => [fn.apply(null, acc)], args)[0]
+
+const doubleSay = str => str + ', ' + str
+const capitalize = str => str[0].toUpperCase() + str.substring(1)
+const exclaim = str => str + '!'
+
+let phrase = doubleSay('hello')
+phrase = capitalize(phrase)
+phrase = exclaim(phrase)
+console.log(phrase)
+
+phrase = exclaim(capitalize(doubleSay('hello')))
+console.log(phrase)
+
+let transform = compose(exclaim, capitalize, doubleSay)
+console.log(transform('hello'))
+
+transform = pipe(doubleSay, capitalize, exclaim)
+console.log(transform('hello'))
+
+pipe = (arg, ...fns) => fns.reduce((acc, fn) => [fn.apply(null, acc)], [arg])[0]
+
+console.log(
+  pipe(
+    'hello', //
+    doubleSay,
+    capitalize,
+    exclaim,
+  ),
+)
+
+/**
+ * console.log(
+ *  'hello'
+ *    |> doubleSay
+ *    |> capitalize
+ *    |> exclaim
+ * )
+ */
+
+console.log(
+  numbers.map(a =>
+    pipe(
+      a, //
+      multiply(5),
+      exclaim,
+    ),
+  ),
+)
+
+//
+
+let fibo = n => {
+  if (n <= 2) return 1
+  let a = 1
+  let b = 1
+  let tmp
+  for (let i = 3; i <= n; i++) {
+    tmp = a + b
+    a = b
+    b = tmp
+  }
+  return b
+}
+console.log(fibo(7))
+
+fibo = n => {
+  if (n <= 2) return 1
+  return fibo(n - 2) + fibo(n - 1)
+}
+console.log(fibo(7))
+
+const quickSort = arr => {
+  if (!arr.length) return arr
+  const [pivot, ...rest] = arr
+  const left = rest.filter(lt(pivot))
+  const right = rest.filter(gt(pivot))
+  return quickSort(left).concat([pivot]).concat(quickSort(right))
+}
+console.log(quickSort([5, 3, 8, 12, 1]))
 
 const tailFibo = (n, a = 0, b = 1) => () => {
   if (n === 0) return a
@@ -33,4 +117,4 @@ const runLazy = fn => {
   }
 }
 
-console.log(runLazy(tailFibo(5)))
+console.log(runLazy(tailFibo(7)))
